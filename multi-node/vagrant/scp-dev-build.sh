af2e41f64b57a92cfd0ca16a813108275bb9c20f
Travis Nielsen <travisn@live.com>
2017-07-11 14:45:16 -0700

    my dev script updates

--- multi-node/vagrant/scp-dev-build.sh
+++ multi-node/vagrant/scp-dev-build.sh
@@ -1,15 +1,19 @@
 apply-docker-image() {
-  tag=$1
-  name=$2
+  base_name=$1
+  tag=$2
+  name=$3
   folder=~/src/go/src/github.com/rook/rook/release
   dockerfile=$folder/rook-$name-dev.docker
-  cp $folder/quay.io-rook-$name-$tag.aci $folder/quay.io-rook-$name-dev.aci
-  cp $folder/rook-$name-$tag.docker $dockerfile
-  echo "applying $name"
+  #cp $folder/quay.io-rook-$name-$tag.aci $folder/quay.io-rook-$name-dev.aci
+  #cp $folder/rook-$name-$tag.docker $dockerfile
+  echo "applying $name at $base_name:$tag"
 
-  docker tag quay.io/rook/$name:$tag quay.io/rook/$name:dev
+  docker tag $base_name:$tag quay.io/rook/$name:dev
+  docker save quay.io/rook/$name:dev -o $dockerfile
+  #~/Downloads/docker2aci $folder/rook-$name-dev.docker
+  #cp rook-$name-dev.aci $folder/
 
-  for vm in c1 w1  ; do vagrant scp $dockerfile $vm:~/. ; done
+  for vm in c1 w1 ; do vagrant scp $dockerfile $vm:~/. ; done
   for vm in c1 w1 ; do vagrant ssh $vm -- docker load -i rook-$name-dev.docker; done
   for vm in c1 w1 ; do vagrant ssh $vm -- docker tag quay.io/rook/$name:$tag quay.io/rook/$name:dev; done
 }
@@ -18,7 +22,8 @@ docker rmi quay.io/rook/rookd:dev
 docker rmi quay.io/rook/rook:dev
 docker rmi quay.io/rook/toolbox:dev
 image_name=$(docker images -a | grep "rookd" -m 1 | awk '{print $2}')
+base_name=$(docker images -a | grep "rookd" -m 1 | awk '{print $1}')
+base_toolbox_name=$(docker images -a | grep "toolbox" -m 1 | awk '{print $1}')
 echo "image = $image_name"
-apply-docker-image $image_name rookd
-apply-docker-image $image_name rook
-apply-docker-image $image_name toolbox
+time apply-docker-image $base_name $image_name rookd
+time apply-docker-image $base_toolbox_name latest toolbox
--- single-node/scp-dev-build.sh
+++ single-node/scp-dev-build.sh
@@ -1,25 +1,28 @@
 apply-docker-image() {
-  tag=$1
-  name=$2
-  folder=~/src/go/src/github.com/rook/rook/release
+  base_name=$1
+  tag=$2
+  name=$3
+  folder=~/src/go/src/github.com/rook/rook/.work
   dockerfile=$folder/rook-$name-dev.docker
-  cp $folder/quay.io-rook-$name-$tag.aci $folder/quay.io-rook-$name-dev.aci
-  cp $folder/rook-$name-$tag.docker $dockerfile
-  echo "applying $name"
+  #cp $folder/quay.io-rook-$name-$tag.aci $folder/quay.io-rook-$name-dev.aci
+  #cp $folder/rook-$name-$tag.docker $dockerfile
+  echo "applying $name at $base_name:$tag"
 
-  docker tag quay.io/rook/$name:$tag quay.io/rook/$name:dev
+  docker tag $base_name:$tag rook/$name:dev
+  docker save rook/$name:dev -o $dockerfile
+  #~/Downloads/docker2aci $folder/rook-$name-dev.docker
+  #cp rook-$name-dev.aci $folder/
 
   for vm in default ; do vagrant scp $dockerfile $vm:~/. ; done
   for vm in default ; do vagrant ssh $vm -- docker load -i rook-$name-dev.docker; done
-  for vm in default ; do vagrant ssh $vm -- docker tag quay.io/rook/$name:$tag quay.io/rook/$name:dev; done
-  #for vm in default ; do vagrant ssh $vm -- docker tag $imagename $imagename:dev; done
+  for vm in default ; do vagrant ssh $vm -- docker tag rook/$name:$tag rook/$name:dev; done
 }
 
-docker rmi quay.io/rook/rookd:dev
-docker rmi quay.io/rook/rook:dev
-docker rmi quay.io/rook/toolbox:dev
-image_name=$(docker images -a | grep "rookd" -m 1 | awk '{print $2}')
+docker rmi rook/rook:dev
+docker rmi rook/toolbox:dev
+image_name=$(docker images -a | grep "rook-amd64" -m 1 | awk '{print $2}')
+base_name=$(docker images -a | grep "rook-amd64" -m 1 | awk '{print $1}')
+base_toolbox_name=$(docker images -a | grep "toolbox" -m 1 | awk '{print $1}')
 echo "image = $image_name"
-apply-docker-image $image_name rookd
-apply-docker-image $image_name rook
-apply-docker-image $image_name toolbox
+time apply-docker-image $base_name $image_name rook
+time apply-docker-image $base_toolbox_name $image_name toolbox
